f8237a887da2b028c81ddd9e54f08f1c8b3aeee4
mathiastraicoff <mathiastraicoff@gmail.com>
2024-09-02 03:19:07 -0300

    v 2.0.0

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
 import express from 'express';
 import productsRouter from './routes/products.router.js'
 import cartsRouter from './routes/carts.router.js';
-
+import __dirname from './utils.js';
+import handlebars from 'express-handlebars'
+// import { server } from 'socket.io'
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
 
 
 const SERVER_PORT = 8080;
+
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set ('view engine','handlebars');
+
+
+app.use(express.static(__dirname + '/public'))
+
+
 app.listen(SERVER_PORT, () => {
-    console.log("Servidor escuchando por el puerto: " + SERVER_PORT);
+    console.log(`Servidor escuchando por el puerto: ${SERVER_PORT}`);
 });
 
 
